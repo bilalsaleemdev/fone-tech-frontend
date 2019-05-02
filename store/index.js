5560fc4de6b4816395ba4179225a617f41e857c1
Anas Majeed <anasmajeed.ch@gmail.com>
2019-05-02 12:17:18 +0500

    NEW WAY OF WRITING STORE

--- store/index.js
+++ store/index.js
@@ -2,50 +2,47 @@ import Vuex from 'vuex'
 import axios from 'axios'
 import Cookie from 'js-cookie'
 
-const createStore = () => {
-    return new Vuex.Store({
-        state:{
-            token:null,
-            user:{}
-        },
-        mutations:{
-            setToken(state,token){
-                state.token = token;
-            },
-            setUser(state,user){
-                state.user = user;
-            }   
-        },
-        actions:{
-            authenticate(vueContext,authData){
-                axios.post('http://localhost:8000/api/login',{
-                    user_name : authData.user_name,
-                    password : authData.password
-                }).then(result => {
-                    vueContext.commit('setToken',result.data.token);
-                    localStorage.setItem('token',result.data.token);
-                    Cookie.set('jwt',result.data.token);
+export const state = () =>({
+    token:null,
+    user:{}
+})
 
-                    axios.get('http://localhost:8000/api/user',{headers:{'Authorization':'Bearer ' + result.data.token}})
-                    .then(user =>{
-                        vueContext.commit('setUser',user.data);
-                        localStorage.setItem('user',user.data);
-                        Cookie.set('user',user.data);
-                    });
-                }).catch(e =>{
-                    console.log(e);
-                });
-
-                
-
-            }
-        },
-        getters:{
-            isAuthenticated(state){
-                return state.token != null;
-            }
-        }
-    });
+export const mutations = {
+    setToken(state,token){
+        state.token = token;
+    },
+    setUser(state,user){
+        state.user = user;
+    }
 }
 
-export default createStore
+export const actions = {
+    authenticate(vueContext,authData){
+        axios.post('http://localhost:8000/api/login',{
+            user_name : authData.user_name,
+            password : authData.password
+        }).then(result => {
+            vueContext.commit('setToken',result.data.token);
+            localStorage.setItem('token',result.data.token);
+            Cookie.set('jwt',result.data.token);
+
+            axios.get('http://localhost:8000/api/user',{headers:{'Authorization':'Bearer ' + result.data.token}})
+            .then(user =>{
+                vueContext.commit('setUser',user.data);
+                localStorage.setItem('user',user.data);
+                Cookie.set('user',user.data);
+            });
+        }).catch(e =>{
+            console.log(e);
+        });
+
+        
+
+    }
+}
+
+export const getters = {
+    isAuthenticated(state){
+        return state.token != null;
+    }
+}
